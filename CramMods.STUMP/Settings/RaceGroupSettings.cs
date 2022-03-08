5e57b5c538de0371c9c6b59909d6c111c5812256
Cram42 <cram42@users.noreply.github.com>
2022-03-08 13:24:10 +0800

    Implement RaceGroups and NPC matching

---
+++ CramMods.STUMP/Settings/RaceGroupSettings.cs
@@ -0,0 +1,11 @@
+﻿using CramMods.NARFI.Skyrim;
+using Mutagen.Bethesda.Skyrim;
+using Newtonsoft.Json;
+
+namespace CramMods.STUMP.Settings
+{
+    public class RaceGroupSettings : Dictionary<string, string[]>
+    {
+        public IEnumerable<RaceGroup> Flatten(List<IRaceGetter> allRaces) => RaceGroup.FromIdDictionary(this, allRaces);
+    }
+}
