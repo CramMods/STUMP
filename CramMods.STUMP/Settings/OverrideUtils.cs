5c43f11ba1490a0996852a07c1754e6e9cd51b4e
Cram42 <cram42@users.noreply.github.com>
2022-03-08 10:28:01 +0800

    Add OverrideUtils.Merge

---
+++ CramMods.STUMP/Settings/OverrideUtils.cs
@@ -0,0 +1,26 @@
+﻿namespace CramMods.STUMP.Settings
+{
+    public static class OverrideUtils
+    {
+        public static List<Override>? Merge(List<Override> overrides1, List<Override> overrides2)
+        {
+            List<Override> output = overrides1.Select(o => o.Clone()).ToList();
+
+            foreach (Override over2 in overrides2)
+            {
+                Override? matching = output.FirstOrDefault(o => (o.Type == over2.Type) && (o.Part == over2.Part))?.Clone();
+                if (matching == null)
+                {
+                    matching = new() { Part = over2.Part, Type = over2.Type };
+                    output.Add(matching);
+                }
+                if (matching.Lock) return null;
+
+                matching.Path = over2.Path;
+                matching.Lock = over2.Lock;
+            }
+
+            return output;
+        }
+    }
+}
