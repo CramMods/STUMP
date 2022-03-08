de8b70de21aa7226199e7cd0041075a54ed73d39
Cram42 <cram42@users.noreply.github.com>
2022-03-08 10:27:41 +0800

    Add Override Lock and Clone()

--- CramMods.STUMP/Settings/Override.cs
+++ CramMods.STUMP/Settings/Override.cs
@@ -14,6 +14,22 @@ namespace CramMods.STUMP.Settings
 
         private string _path = string.Empty;
         public string Path { get => _path; set => _path = value; }
+
+        private bool _lock = false;
+        public bool Lock { get => _lock; set => _lock = value; }
+
+        public override string ToString() => $"{_part} {_type} {_path}";
+
+        public Override() { }
+        public Override(Override existing)
+        {
+            _part = existing.Part;
+            _type = existing.Type;
+            _path = existing.Path;
+            _lock = existing.Lock;
+        }
+
+        public Override Clone() => new(this);
     }
 
     public class OverrideJsonConverter : JsonConverter<Override>
@@ -29,6 +45,7 @@ namespace CramMods.STUMP.Settings
             Override o = new();
             if (items.ContainsKey("part")) o.Part = items["part"].ToObject<OverridePart>(serializer);
             if (items.ContainsKey("type")) o.Type = items["type"].ToObject<OverrideType>(serializer);
+            if (items.ContainsKey("lock")) o.Lock = items["lock"].ToObject<bool>(serializer);
             if (items.ContainsKey("path")) o.Path = items["path"].ToObject<string>(serializer) ?? throw new JsonSerializationException("Expected path to be a string");
 
             return o;
